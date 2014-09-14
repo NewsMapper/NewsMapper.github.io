b176928c2c8a4b768c8611dacf3ffa3834b68d24
gdyer <9fermat@gmail.com>
2014-09-14 18:51:36 -0500

    small change

--- Resources/pull.py
+++
@@ -1,8 +0,0 @@
-import os
-from os.path import expanduser
-home = expanduser("~")
-
-to_cd = home + "/Desktop/newsmapper.github.io"
-print(to_cd)
-os.chdir(to_cd)
-os.system("git pull https://github.com/newsmapper/newsmapper.github.io")
