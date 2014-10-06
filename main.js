ad26b445198a8cd06561d58105a8755b1978cb06
gdyer <9fermat@gmail.com>
2014-10-05 22:36:00 -0500

    no parsing done yet, but interface is mostly done

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
   $('#map-canvas').css('height', $(window).height() - 48 + 'px');
   var options = {
-      zoom: 4,
+      zoom: 16,
       maxZoom: 20,
       minZoom: 4,
-      center: new google.maps.LatLng(38.251123,-97.998047),
-      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      center: new google.maps.LatLng(40.105876,-88.228111),
+      mapTypeId: google.maps.MapTypeId.HYBRID,
       styles: [{"featureType":"road","elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#fffffa"}]},{"featureType":"water","stylers":[{"lightness":50}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"transit","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry","stylers":[{"lightness":40}]}]
   };
 
@@ -18,31 +18,57 @@ $(document).ready(function(){
 
       var marker = new google.maps.Marker({
           position: latlng,
-          map: map,
+          map: map
       });
 
       google.maps.event.addListener(marker, 'click', function() {
               infowindow.open(map, this);
         });
 
-      if ($(window).width() > 500) {
-        google.maps.event.addListener(marker, 'mouseout', function() {
-              infowindow.close();
-        });
-      }
+      // if ($(window).width() > 500) {
+      //   google.maps.event.addListener(marker, 'mouseout', function() {
+      //         infowindow.close();
+      //   });
+      // }
 
   }
 
   function initialize() {
+    var stDate;
+    var now;
+    var assoc = new Array(7);
+    assoc[0] = "Sunday";
+    assoc[1] = "Monday";
+    assoc[2] = "Tuesday";
+    assoc[3] = "Wednesday";
+    assoc[4] = "Thursday";
+    assoc[5] = "Friday";
+    assoc[6] = "Saturday";
+    now = new Date();
+    stDate = now.getDay();
+    DayOfWeek = (assoc[stDate]);
+
     var points = [
       {
-        "lat" : 40.804333,
-        "lon" : -74.448922,
-        "title" : "Herro der fwend."
+        "lat" : 40.114066,
+        "lon" : -88.224699,
+        "title" : "But if you tell us your email address above, we'll keep you posted."
       },{
-        "lat" : 40.712643,
-        "lon" : -74.3546,
-        "title" : "How are you?"
+        "lat" : 40.112409,
+        "lon" : -88.226836,
+        "title" : "We're not ready for public consumption yet."
+      },{
+        "lat" : 40.109307,
+        "lon" : -88.228347,
+        "title" : "Hello, welcome to NewsMapper."
+      },{
+        "lat" : 40.110858,
+        "lon" : -88.226976,
+        "title" : "We're part of CS 196 at UIUC (if you couldn't recognize the area)"
+      },{
+        "lat" : 40.115699,
+        "lon" : -88.227291,
+        "title" : "In the meantime, enjoy the rest of your " + DayOfWeek + "!"
       }
     ];
 
@@ -58,56 +84,29 @@ $(document).ready(function(){
   });
 
   $('body').click(function(e) {
-    if (!$(e.target).is('#field')) {
+    var res = window.location.search;
+    if (!$(e.target).is('#field') && res != "?s" && res != '?e') {
         $('#field').attr('placeholder', 'subscribe to updates');
     }
   });
 
   var res = window.location.search;
   if (res == "?s") {
-    $('#messages').flash_message({
-        text: 'You\'re on our list! Thank you for your time; we promise we\'ll put it to use!',
-        how: 'append'
-    });
+        $('#field').attr('placeholder', 'we got it; thank you!');
+        $('#field').css('background-color', '#003C7D');
+        $('#field').css('border', '1px solid #fff');
+        $("#field").prop('disabled', true);
+        $('#field').css('cursor', 'not-allowed');
   } else if (res == "?f") {
-    document.getElementById("message").innerHTML = "Oops! There was a problem processing that address; please try again.";
-    $('#message').removeClass('text-primary');
-    $('#message').addClass('text-danger');
+        $('#field').attr('placeholder', 'oops; please try again!');
+        $('#field').css('background-color', '#003C7D');
+        $('#field').css('border', '1px solid #F47F24');
   } else if (res == "?e") {
-    document.getElementById("message").innerHTML = "Oops! There's a problem with our script. Please <a href='mailto:team@gdyer.de'>email us</a> ASAP!";
-    $('#message').removeClass('text-primary');
-    $('#message').addClass('text-warning');
+        $('#field').attr('placeholder', 'error; tell team@news.gdyer.de!');
+        $('#field').css('background-color', '#003C7D');
+        $('#field').css('width', '200px');
+        $('#field').css('border', '1px solid #F47F24');
+        $("#field").prop('disabled', true);
+        $('#field').css('cursor', 'not-allowed');
   }
-
-
-  (function($) {
-      $.fn.flash_message = function(options) {
-        
-        options = $.extend({
-          text: 'Done',
-          time: 1000,
-          how: 'before',
-          class_name: ''
-        }, options);
-        
-        return $(this).each(function() {
-          if( $(this).parent().find('.flash_message').get(0) )
-            return;
-          
-          var message = $('<span />', {
-            'class': 'flash_message ' + options.class_name,
-            text: options.text
-          }).hide().fadeIn('fast');
-          
-          $(this)[options.how](message);
-          
-          message.delay(options.time).fadeOut('normal', function() {
-            $(this).remove();
-          });
-          
-        });
-      };
-  })(jQuery);
-
-
 });
